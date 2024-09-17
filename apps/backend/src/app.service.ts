8f64f8dabbbbd4beee6b876df00f33955280b923
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-09-17 12:06:55 +0545

    refactor: Update app.controller.ts and app.service.ts
    Refactor the code in app.controller.ts and app.service.ts to improve the health endpoint.

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getStatus(): { status: string; statusCode: number } {
+    return {
+      status: 'Ok',
+      statusCode: HttpStatus.OK,
+    };
   }
 }
