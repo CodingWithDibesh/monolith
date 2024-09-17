4949030b5321014df008f309f440581b8d4a8c16
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-09-17 12:07:03 +0545

    refactor: Update app.module.ts and main.ts
    - Update app.module.ts to import ConfigModule from '@nestjs/config' and make it global.
    - Update main.ts to use the ConfigService from '@nestjs/config' to get the port number from the environment variables and dynamically set it for the application to listen on.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3001);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT') || 7002;
+
+  await app.listen(port);
+  console.log(
+    `Application running on port [${port}]\n http://localhost:${port}`,
+  );
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
