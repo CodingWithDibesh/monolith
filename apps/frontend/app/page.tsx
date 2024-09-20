99d2fdd8697727eb0ab5a16310872050c2863d28
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-09-20 22:24:06 +0545

    refactor: Update ESLint configuration and add test page
    - Update ESLint configuration to fix a typo in the extends array
    - Add a test page to the frontend with a button and some text
    - Enable trailing slash in the Next.js configuration

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -2,8 +2,15 @@ import { Button } from "@repo/src/Atoms"
 
 export default function Home() {
   return (
-    <div className="w-full h-screen p-4 bg-gray-800">
-       <Button label="Open alert" />
-    </div>
+    <>
+      <div className="w-full h-screen p-4 bg-gray-800">
+        <div className="flex flex-col gap-4">
+          <div className="text-white">
+            This Is a test Page for the frontend
+          </div>
+          <Button label="Open alert" />
+        </div>
+      </div>
+    </>
   );
 }
